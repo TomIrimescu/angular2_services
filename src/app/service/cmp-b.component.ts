e6d0be708a91eacc370bc2c2742f8d7a5a9df216
TomIrimescu <tom@irimescu.com>
2016-08-29 10:04:28 -0400

    Updated cmp-a and cmp-b templates

--- src/app/service/cmp-b.component.ts
+++ src/app/service/cmp-b.component.ts
@@ -4,14 +4,37 @@ import { Component, OnInit } from '@angular/core';
   moduleId: module.id,
   selector: 'app-cmp-b',
   template: `
-    <p>
-      cmp-b Works!
-    </p>
+    <div>
+      <input type="text" #input>
+      <button (click)="onLog(input.value)">Log</button>
+      <button (click)="onStore(input.value)">Store</button>
+    </div>
+    <hr>
+    <div>
+        <button (click)="onGet()">Refresh Storage</button>
+        <h3>Storage</h3>
+        <ul>
+            <li *ngFor="let item of items">{{item}}</li>
+        </ul>
+        <h3>Received Value</h3>
+        <p>{{value}}</p>
+    </div>
   `
 })
 export class CmpBComponent implements OnInit {
+  value = '';
+  items: string[] = [];
 
-  ngOnInit() {
+  onLog(value: string) {
   }
 
+  onStore(value: string) {
+  }
+
+  onGet() {
+  }
+
+  ngOnInit() {
+
+  }
 }
